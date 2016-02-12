7d92a57154945ff5b3b4343cf9997617dcc9f67d
karan <bamniyakaran71@gmail.com>
2016-02-12 12:48:46 +0530

    devise and role defined

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,28 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+
+
+
+
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "gmail.com",
+      :user_name => "bamniyakaran71@gmail.com",
+      :password => "87mymanishak",
+      :authentication => "plain",
+      :enable_starttls_auto => true
+  }
+
+
+
+
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  
+
+  get 'users/signin'
+
+  #get 'users/index'
+
+  
+ # root to: 'welcome#index'
+root to: 'users#index'
+devise_for :users, :controllers => { :registrations => "users/registrations" }
+ # devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
