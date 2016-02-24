a86d51bcc68496a7f5e04d0519b519cb40de1d15
manoj-btc <manojparmar5606@gmail.com>
2016-02-24 14:59:04 +0530

    activate/deactivate user admin functionality with ajax

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -11,24 +11,30 @@ class AdminsController < ApplicationController
 	end
 
 	def view_cases
-		@cases = Case.order(:case_title).paginate(:page => params[:page], :per_page => 5)
+		@case1 = Case.order(:case_title).paginate(:page => params[:page], :per_page => 5)
 	end
 
 	def view_case
 	end
 
 	def update_user
-		@user.update!(is_active: !@user.is_active)
-		if @user.is_advocate?
-			redirect_to  admins_view_advocates_path 
-	  else
-			redirect_to  admins_view_clients_path 
-	  end
+		@user.update(is_active: !@user.is_active)
+			if @user.is_advocate? 
+				@users = User.joins(:user_profile).order("user_profiles.first_name").where(role_id: Role.find_by(title: 'advocate').id).paginate(:page => params[:page], :per_page => 5)
+			else
+				@users = User.joins(:user_profile).order("user_profiles.first_name").where(role_id: Role.find_by(title: 'client').id).paginate(:page => params[:page], :per_page => 5)
+			end
+		respond_to do |format|
+		format.js
+		end
 	end
 
 	def update_case
-		@c.update!(is_active: !@c.is_active)
-		redirect_to admins_view_cases_path 
+		@case1.update(is_active: !@case1.is_active)
+		@case1 = Case.order(:case_title).paginate(:page => params[:page], :per_page => 5)
+		respond_to do |format|
+		format.js
+		end
 	end
 
 	private
@@ -37,6 +43,6 @@ class AdminsController < ApplicationController
 	end
 
 	def set_case
-		@c = Case.find(params[:id])
+		@case1 = Case.find(params[:id])
 	end
 end
