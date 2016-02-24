48c7e3d6dbc95b14185e752ac44a6729cb531334
karan <bamniyakaran71@gmail.com>
2016-02-24 19:39:48 +0530

    merge of remaining

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-<<<<<<< HEAD
 	has_many :cases, dependent: :destroy
 	devise :database_authenticatable, :registerable, :recoverable,
 				 :rememberable, :trackable, :validatable, :confirmable, :lockable
@@ -12,27 +11,10 @@ class User < ActiveRecord::Base
 	end
 
 	def is_client?
-  	self.role.title == Role::CLIENT
-  end
+  	   self.role.title == Role::CLIENT
+    end
 
 	def is_advocate?
-  	self.role.title == Role::ADVOCATE
-=======
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
-  belongs_to :role, dependent: :destroy
-  has_one :user_profile
-  ratyrate_rater
-  def is_system_admin?
-   self.role.title == Role::SYSTEM_ADMIN ? true : false
-  end
-
-  def is_client?
-        self.role.title == Role::CLIENT ? true :false
-  end
-
-  def is_advocate?
-        self.role.title == Role::ADVOCATE ? true :false
->>>>>>> kb-issue-15.dev
-  end
+  	   self.role.title == Role::ADVOCATE
+    end
 end
