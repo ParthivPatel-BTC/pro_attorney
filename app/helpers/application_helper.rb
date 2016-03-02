c6323cdc2089179718419fbc32a6d37e3bee801a
manoj-btc <manojparmar5606@gmail.com>
2016-03-02 13:39:04 +0530

    comm

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 	end
 end
 def avatar_display(options)
-	if current_user.user_profile == nil
+	if current_user.user_profile == nil || current_user.user_profile.avatar_file_name == "default.jpg"
 		image_tag("default.jpg",{height: 20,width: 20,class: "user-image"})
 	else
 		image_tag(current_user.user_profile.avatar.url(:small),options)
