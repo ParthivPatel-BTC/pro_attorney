67a8d78205914aac80cddfc8cab7f1ac9c4e3367
manoj-btc <manojparmar5606@gmail.com>
2016-03-12 11:54:02 +0530

    ajax searchbox and user_log report

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,8 +64,8 @@ config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   address:              'smtp.gmail.com',
   port:                 '587',
   domain:               'gmail.com',
-  :user_name          => 'gauravmakwana93@gmail.com',
-  :password           => 'shivgaurav11',
+  user_name:            'proattorney@gmail.com',
+  password:             'indiapro',
   authentication:       'login',
   enable_starttls_auto: true  
   }
