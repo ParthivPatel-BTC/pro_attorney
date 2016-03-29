9c9aa4c8b65a3c792c83da3938323cce18c8139d
Naiya <naiya.d.shah@gmail.com>
2016-03-29 16:35:29 +0530

    bootstrap gem added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery-ui
 //= require jquery_ujs
 //= require jquery.raty
-//= require bootstrap
+//= require bootstrap.min
 //= require ratyrate
 //= require app.min
